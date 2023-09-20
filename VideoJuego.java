124d0ac6fb48cff6d96aad748a09a6e635c5f38c
mvelasquea <144069262+mvelasquea@users.noreply.github.com>
2023-09-20 14:28:43 -0500

    Add files via upload

---
+++ VideoJuego.java
@@ -0,0 +1,32 @@
+public class VideoJuego {
+        public static void main(String args[]){
+                System.out.println("Bienvenidos a mi VideoJuego");
+
+		Scanner entrada=new Scanner  (System.in);
+	String soldado01;
+	String soldado02;
+	String soldado03;
+	String soldado04;
+	String soldado05;
+	int i=1;
+System.out.println("ingrese el nombre del soldado "+i++);
+soldado01=entrada.nextLine();
+System.out.println("ingrese el nombre del soldado "+i++);
+soldado02=entrada.nextLine();
+System.out.println("ingrese el nombre del soldado "+i++);
+soldado03=entrada.nextLine();
+System.out.println("ingrese el nombre del soldado "+i++);
+soldado04=entrada.nextLine();
+System.out.println("ingrese el nombre del soldado "+i++);
+soldado05=entrada.nextLine();
+int j=1;
+System.out.println("el nombre de los 5 soldados es: ");
+System.out.println(j++ +"-"+soldado01);
+System.out.println(j++ +"-"+soldado02);
+System.out.println(j++ +"-"+soldado03);
+System.out.println(j++ +"-"+soldado04);
+System.out.println(j++ +"-"+soldado05);
+
+	}
+	}
+
