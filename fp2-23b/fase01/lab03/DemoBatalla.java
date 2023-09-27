96bcf26a8b657a06ca045b376280f506959e0dc4
mvelasquea@unsa.edu.pe <mvelasquea@unsa.edu.pe>
2023-09-27 08:31:59 -0500

    Demo batalla terminado

--- fp2-23b/fase01/lab03/DemoBatalla.java
+++ fp2-23b/fase01/lab03/DemoBatalla.java
@@ -1,25 +1,23 @@
 public class DemoBatalla {
-
-	public static void main(String[] args) {
-		Scanner objeto = new Scanner(System.in);
-		Nave[] misNaves = new Nave[2];
+	public static void main(String [] args){
+		Nave [] misNaves = new Nave[10];
+		Scanner sc = new Scanner(System.in);
 		String nomb, col;
 		int fil, punt;
 		boolean est;
 		for (int i = 0; i < misNaves.length; i++) {
-		System.out.println("\033[31mNave " + (i + 1));
-		System.out.print("\033[32mNombre: ");
-		nomb = objeto.next();
-		System.out.println("\033[35mFila ");
-		fil = objeto.nextInt();
-		System.out.print("\033[36mColumna: ");
-		col = objeto.next();
-		System.out.print("\033[31mEstado: ");
-		est = objeto.nextBoolean();
-		System.out.print("\033[32mPuntos: ");
-		punt = objeto.nextInt();
-		misNaves[i] = new Nave();
-		//Se crea un objeto Nave y se asigna su referencia a misNaves
+		System.out.println("Nave " + (i+1));
+		System.out.print("Nombre: ");
+		nomb = sc.next();
+		System.out.println("Fila ");
+		fil = sc.nextInt();
+		System.out.print("Columna: ");
+		col = sc.next();
+		System.out.print("Estado: ");
+		est = sc.nextBoolean();
+		System.out.print("Puntos: ");
+		punt = sc.nextInt();
+		misNaves[i] = new Nave(); //Se crea un objeto Nave y se asigna su referencia a misNaves
 		misNaves[i].setNombre(nomb);
 		misNaves[i].setFila(fil);
 		misNaves[i].setColumna(col);
@@ -33,31 +31,72 @@ public class DemoBatalla {
 		System.out.println("\nNave con mayor número de puntos: " + mostrarMayorPuntos(misNaves));
 		}
 		//Método para mostrar todas las naves
-		public static void mostrarNaves(Nave[] flota) {
-		for (int i = 0; i < flota.length; i++) {
-		Nave nave = flota[i];
-		}}
+		public static void mostrarNaves(Nave [] flota){
+			for (int i = 0; i < flota.length; i++) {
+				System.out.println("NOMBRE: " +flota[i].getNombre());
+				System.out.println("FILA: " +flota[i].getFila());
+				System.out.println("COLUMNA: " +flota[i].getColumna());
+				if (flota[i].getEstado()==true) {
+				System.out.println("vivo");
+				} else {
+				System.out.println("muerto");
+				}
+				System.out.println("PUNTOS DE VIDA: "+flota[i].getPuntos());
+				}}
 		//Método para mostrar todas las naves de un nombre que se pide por teclado
-		public static void mostrarPorNombre(Nave[] flota) {
-		for (int i = 0; i < flota.length; i++) {
-		Nave nave = flota[i];
-		}
-		}
+		public static void mostrarPorNombre(Nave [] flota){
+			String nombre;
+			System.out.println("INGRESAR EL NOMBRE DEL QUE SE BUSCA");
+			Scanner objeto= new Scanner(System.in);
+			nombre=objeto.nextLine();
+			for (int i = 0; i < flota.length; i++) {
+			if (flota[i].getNombre().equals(nombre)) {
+			System.out.println("COINCIDENCIA CON: "+nombre);
+			System.out.println("NOMBRE: " +flota[i].getNombre());
+			System.out.println("FILA: " +flota[i].getFila());
+			System.out.println("COLUMNA: " +flota[i].getColumna());
+			if (flota[i].getEstado()==true) {
+			System.out.println("vivo");
+			} else {
+			System.out.println("esta muerto");
+			}
+			System.out.println("PUNTOS DE VIDA: "+flota[i].getPuntos());
+			break;
+			}else{
+			System.out.println("---");
+			}}}
 		//Método para mostrar todas las naves con un número de puntos inferior o igual
 		//al número de puntos que se pide por teclado
-		public static void mostrarPorPuntos(Nave[] flota) {
-		for (int i = 0; i < flota.length; i++) {
-		Nave nave = flota[i];
-		}
-		}
+		public static void mostrarPorPuntos(Nave [] flota){
+			for (int i = 0; i < flota.length; i++) {
+				int puntoComparacion;
+				System.out.println("INGRESE LA CANTIDAD DE VIDA QUE DESEA COMPARAR");
+				Scanner objeto= new Scanner(System.in);
+				puntoComparacion=objeto.nextInt();
+				for (int j = 0; j < flota.length; j++) {
+				if (flota[i].getPuntos()<=puntoComparacion) {
+				System.out.println("NOMBRE: " +flota[i].getNombre());
+				System.out.println("FILA: " +flota[i].getFila());
+				System.out.println("COLUMNA: " +flota[i].getColumna());
+				if (flota[i].getEstado()==true) {
+				System.out.println("vivo");
+				} else {
+				System.out.println("muerto");
+				}
+				System.out.println("PUNTOS DE VIDA: "+flota[i].getPuntos());
+				}else{
+				System.out.println("---");
+				}}}}
 		//Método que devuelve la Nave con mayor número de Puntos
-		public static Nave mostrarMayorPuntos(Nave[] flota) {
-		for (int i = 0; i < flota.length; i++) {
-		Nave nave = flota[i];
+
+			public static Nave mostrarMayorPuntos(Nave[] flota) {
+			    Nave naveMayor = flota[0]; // Supongamos que la primera nave tiene la mayor cantidad de puntos inicialmente
+			    for (int i = 1; i < flota.length; i++) {
+			        if (flota[i].getPuntos() > naveMayor.getPuntos()) {
+			            naveMayor = flota[i];
+			        }
+			    }
+			    return naveMayor;
+			}
 		}
-		
-		return null;
-		}
-		//Crear un método que devuelva un nuevo arreglo de objetos con todos los objetos previamente ingresados
-		//pero aleatoriamente desordenados
-		}
+
