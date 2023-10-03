fbec940114ebc056803b0392dd2a6f830e981df1
mvelasquea@unsa.edu.pe <mvelasquea@unsa.edu.pe>
2023-10-03 13:58:21 -0500

    actualizacion de la clase soldado

--- fp2-23b/fase01/lab05/Soldado.java
+++ fp2-23b/fase01/lab05/Soldado.java
@@ -1,2 +1,40 @@
-inicio de la clase
 
+public class soldado {
+		/*
+		 Reusando el codiogo de los anterioes labs
+		
+			 laboratorio Nro 5 ejercicio 1
+			 //clase soldado
+			 Autor :Mikhail Gabino Velasque Arcos
+			colaboro:---
+			tiempo:
+			 */
+			private String nombre;
+			private int fila;
+			private String columna;
+			private int puntos;
+			public void setNombre( String n){
+			nombre = n;
+			}
+			public void setFila(int f){
+			fila = f;
+			}
+			public void setColumna(String c){
+			columna = c;
+			}
+			public void setPuntos(int p){
+			puntos = p;
+			}
+			public String getNombre(){
+			return nombre;
+	}
+			public int getFila(){
+			return fila;
+	}
+			public String getColumna(){
+			return columna;
+			}
+			public int getPuntos(){
+			return puntos;
+			}}
+	
