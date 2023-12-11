17768989e7e55bde01963df4a71541888e24d598
mvelasquea@unsa.edu.pe <mvelasquea@unsa.edu.pe>
2023-12-10 21:04:28 -0500

    actualizando la clase soldado

--- fp2-23b/fase02/lab12/Soldado.java
+++ fp2-23b/fase02/lab12/Soldado.java
@@ -3,52 +3,178 @@ public class Soldado {
 		/*
 		 Reusando el codiogo de los anterioes labs
 		
-			 laboratorio Nro 8 ejercicio 1
+			 laboratorio Nro 12 ejercicio 1
 			 //clase soldado
 			 Autor :Mikhail Gabino Velasque Arcos
 			colaboro:---
 			tiempo:
 			 */
-	   private String nombre;
-	    private int puntosDeVida;
-	    private int fila;
-	    private int columna;
+		private String nombre;
+		private int nivelAtaque;
+		private int nivelDefensa;
+		private int nivelVida;
+		private int vidaActual;
+		private int velocidad;
+		private int fila;
+		private int columna;
+		private String actitud;
+		private boolean vive;
 
-	    public Soldado(String nombre, int puntosDeVida) {
-	        this.nombre = nombre;
-	        this.puntosDeVida = puntosDeVida;
-	    }
+		public Soldado(String nombre, int fila, int columna){
+			this.nombre = nombre;
+			this.fila = fila;
+			this.columna = columna;
+			int vAleat = (int)(Math.random() * 5 + 1);
+			vidaActual = vAleat;
+			vAleat = (int)(Math.random() * 5 + 1);
+			nivelAtaque = vAleat;
+			vAleat = (int)(Math.random() * 5 + 1);
+			nivelDefensa = vAleat;
+			actitud = "defensiva";
+			vive = true;
+		}
+		public Soldado(){
+			nombre = "Todos";
+			nivelAtaque = 0;
+			nivelDefensa = 0;
+			nivelVida = 0;
+			fila = -1;
+			columna = -1;
+		}
+		public void atacar(){
+			actitud = "ofensiva";
+			avanzar();
+			velocidad++;
+		}
+		public void defender(){
+			actitud = "defensiva";
+			velocidad = 0;
+		}
+		public void avanzar(){
+		
+		}
+		public void retroceder(){
+			if(velocidad > 0){
+				defender();
+			}else{
+				velocidad--;
+			}
+		}
+		public void serAtacado(){
+			vidaActual--;
+			if(vidaActual <= 0){
+				morir();
+			}
+		}
+		public void huir(){
+			actitud = "fuga";
+		}
+		public void morir(){
+			vive = false;
+		}
+		public void setVidaActual(int n){
+			vidaActual = n;
+		}
+		public int getVidaActual(){
+			return vidaActual;
+		}
+		public boolean estaVivo(){
+			return vive;
+		}
+		public String getNombre(){
+			return nombre;
+		}
+		public int getFila(){
+			return fila;
+		}
+		public int getColumna(){
+			return columna;
+		}
+		public void setFila(int f){
+			fila = f;
+		}
+		public void setColumna(int c){
+			columna = c;
+		}
 
-	    public String getNombre() {
-	        return nombre;
-	    }
+		public Soldado clonarSoldado() {
+	    		Soldado nuevoSoldado = new Soldado(nombre, fila, columna);
+	    		nuevoSoldado.nivelAtaque = nivelAtaque;
+	   		nuevoSoldado.nivelDefensa = nivelDefensa;
+	    		nuevoSoldado.nivelVida = nivelVida;
+	    		nuevoSoldado.vidaActual = vidaActual;
+	    		nuevoSoldado.velocidad = velocidad;
+	    		nuevoSoldado.actitud = actitud;
+	    		nuevoSoldado.vive = vive;
+	    		return nuevoSoldado;
+		}
 
-	    public int getPuntosDeVida() {
-	        return puntosDeVida;
-	    }
+		public String toString() {
+			char[] aux = {'A','B','C','D','E','F','G','H','I','J'};
+	    	return "Nombre: " + nombre + "\n" +
+	          	 	"Nivel de Ataque: " + nivelAtaque + "\n" +
+	           		"Nivel de Defensa: " + nivelDefensa + "\n" +
+	           		"Vida Actual: " + vidaActual + "\n" +
+	           		"Velocidad: " + velocidad + "\n" +
+	          		"Fila: " + (fila+1) + "\n" +
+	           		"Columna: " + aux[columna] + "\n" +
+	           		"Actitud: " + actitud + "\n" +
+	           		"Vive: " + vive + "\n";
+		}
+		public void setNivelAtaque(int n){
+			nivelAtaque = n;
+		}
+		public void setNivelDefensa(int n){
+			nivelDefensa = n;
+		}
+		public int getNivelAtaque(){
+			return nivelAtaque;
+		}
+		public int getNivelDefensa(){
+			return nivelDefensa;
+		}
+		public int getNivelVida(){
+			return nivelVida;
+		}
+		public void comparar(Soldado otroSoldado) {
+		    System.out.println("Comparación de atributos con " + otroSoldado.getNombre() + ":");
 
-	    public void setPuntosDeVida(int puntosDeVida) {
-	        this.puntosDeVida = puntosDeVida;
-	    }
+		    if (!this.nombre.equals(otroSoldado.getNombre())) {
+			System.out.println("Nombre: Diferente.");
+		    } else {
+			System.out.println("Nombre: Igual.");
+		    }
 
-	    public int getFila() {
-	        return fila;
-	    }
+		    if (this.nivelAtaque == otroSoldado.getNivelAtaque()) {
+			System.out.println("Nivel de Ataque: Igual.");
+		    } else {
+			System.out.println("Nivel de Ataque: Diferente.");
+		    }
 
-	    public void setFila(int fila) {
-	        this.fila = fila;
-	    }
+		    if (this.nivelDefensa == otroSoldado.getNivelDefensa()) {
+			System.out.println("Nivel de Defensa: Igual.");
+		    } else {
+			System.out.println("Nivel de Defensa: Diferente.");
+		    }
 
-	    public int getColumna() {
-	        return columna;
-	    }
+		    if (this.vidaActual == otroSoldado.getVidaActual()) {
+			System.out.println("Vida Actual: Igual.");
+		    } else {
+			System.out.println("Vida Actual: Diferente.");
+		    }
 
-	    public void setColumna(int columna) {
-	        this.columna = columna;
-	    }
+		    if (this.vive == otroSoldado.estaVivo()) {
+			System.out.println("Vive: Igual.");
+		    } else {
+			System.out.println("Vive: Diferente.");
+		    }
+		}
+		public Soldado sumar(Soldado otroSoldado) {
+			this.nivelAtaque += otroSoldado.nivelAtaque;
+			this.nivelDefensa += otroSoldado.nivelDefensa;
+			this.vidaActual += otroSoldado.vidaActual;
+			this.velocidad += otroSoldado.velocidad;
+			return this;
+	    	}
 
-	    public String toString() {
-	        return "Nombre: " + nombre + ", Vida: " + puntosDeVida + ", Fila: " + fila + ", Columna: " + columna;
-	    }
 	}
-
