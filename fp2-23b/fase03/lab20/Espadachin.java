86a75e3a42ac9b9f1d35e263d8db9eb51e289169
mvelasquea@unsa.edu.pe <mvelasquea@unsa.edu.pe>
2024-01-10 10:16:33 -0500

    agregando la clase derivada de soldado (espadachin)

--- fp2-23b/fase03/lab20/Espadachin.java
+++ fp2-23b/fase03/lab20/Espadachin.java
@@ -1,1 +1,33 @@
-clase Espadachin	
+public class Espadachin extends Soldado {
+	/*
+	Ejercicio lab 20 
+	>	clase derivada de Soldado (Espadachin) 
+	Autor :Mikhail Gabino Velasque Arcos
+	tiempo:---
+	*/
+    private int longitudEspada;
+    public Espadachin(String nombre, int fila, int columna){
+
+        super(nombre, fila, columna);
+
+        int numeroAleatorio = (int)(Math.random() * 2 + 3);
+
+		setVidaActual(numeroAleatorio);
+        longitudEspada = (int)(Math.random()*10 + 1);
+    }
+    public void crearMuroEscudos(){
+        defender();
+    }
+    public String impresionTabla(){
+        return "E"+this.getVidaActual();
+    }
+    public String toString(){
+        return "Nombre: " + nombre + "\n" +
+        "Nivel de Ataque: " + nivelAtaque + "\n" +
+        "Nivel de Defensa: " + nivelDefensa + "\n" +
+        "Vida Actual: " + vidaActual + "\n";
+    }
+}
+
+
+
