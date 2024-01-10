a08facf28333888d3329fcd6b32012d02359080b
mvelasquea@unsa.edu.pe <mvelasquea@unsa.edu.pe>
2024-01-10 10:04:08 -0500

    agregando la extencion de soldado (lancero)

--- fp2-23b/fase03/lab20/Lancero.java
+++ fp2-23b/fase03/lab20/Lancero.java
@@ -1,1 +1,30 @@
- creacion etc etc etc 
+
+public class Lancero extends Soldado{
+		/*
+		 * 	Ejercicio lab 20 
+		 * 		>	clase derivada de Soldado (Lancero) 
+		 * 			Autor :Mikhail Gabino Velasque Arcos
+		 * 				tiempo:---
+		 * 					*/
+	    private int longitudLanza;
+	        public Lancero(String nombre, int fila, int columna){
+			        super(nombre, fila, columna);
+				        int numeroAleatorio = (int)(Math.random() * 2 + 1);
+							setVidaActual(numeroAleatorio);
+							        longitudLanza = (int)(Math.random()*10 + 1);
+								    }
+		    public void schiltrom(){
+			            setNivelDefensa(getNivelDefensa()+1);
+				        }
+		        public String impresionTabla(){
+				        return "L"+this.getVidaActual();
+					    }
+			    public String toString(){
+				            return "Nombre: " + nombre + "\n" +
+						            "Nivel de Ataque: " + nivelAtaque + "\n" +
+							            "Nivel de Defensa: " + nivelDefensa + "\n" +
+								            "Vida Actual: " + vidaActual + "\n";
+					        }
+}
+    
+
