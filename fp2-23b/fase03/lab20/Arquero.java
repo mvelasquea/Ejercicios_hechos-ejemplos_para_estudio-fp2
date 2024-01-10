9bf66f1ead5fac5f65b98d8db280029dd6557fe3
mvelasquea@unsa.edu.pe <mvelasquea@unsa.edu.pe>
2024-01-10 10:07:13 -0500

    agregando la clase extendida de soldado ( arquero)

--- fp2-23b/fase03/lab20/Arquero.java
+++ fp2-23b/fase03/lab20/Arquero.java
@@ -1,1 +1,37 @@
-creando la clase Arquero para su proximo uso
+
+public class Arquero extends Soldado{
+	/*
+	Ejercicio lab 20 
+	>	clase derivada de Soldado (Arquero) 
+	Autor :Mikhail Gabino Velasque Arcos
+	tiempo:---
+	*/
+    private int numFlechas;
+    public Arquero(String nombre, int fila, int columna){
+        super(nombre, fila, columna);
+        int numeroAleatorio = (int)(Math.random() * 3 + 1);
+		setVidaActual(numeroAleatorio);
+        numFlechas = (int)(Math.random()* 1000);
+    }
+    public void disparar(){
+        numFlechas--;
+        if(numFlechas == 0){
+            System.out.println("No se puede atacar");
+        }else{
+            atacar();
+        }
+        
+    }
+    public String impresionTabla(){
+        return "A"+this.getVidaActual();
+    }
+    public String toString(){
+        return "Nombre: " + nombre + "\n" +
+        "Nivel de Ataque: " + nivelAtaque + "\n" +
+        "Nivel de Defensa: " + nivelDefensa + "\n" +
+        "Vida Actual: " + vidaActual + "\n";
+    }
+}
+
+
+
