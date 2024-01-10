1898227f15e37355498a74cd68f4eca77a1d9d30
mvelasquea@unsa.edu.pe <mvelasquea@unsa.edu.pe>
2024-01-10 10:38:16 -0500

    finalizando el codigo con la clase principal

--- fp2-23b/fase03/lab20/Videojuego.java
+++ fp2-23b/fase03/lab20/Videojuego.java
@@ -1,1 +1,54 @@
-ase videojuego
+
+import java.util.*;
+
+public class Videojuego{
+	/*
+	Ejercicio lab 20 
+	>	clase main 
+	Autor :Mikhail Gabino Velasque Arcos
+
+	tiempo:---
+	*/
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		String end = "";
+		do{
+			Mapa terreno = new Mapa();
+			terreno.genTablero();
+			terreno.imprimirMapa();
+			System.out.println("Ejercito 1");
+			System.out.println("\u001B[32m" + terreno.getEjercito1() + "\u001B[0m");
+			System.out.println("Ejercito 2");
+			System.out.println("\u001B[31m" +terreno.getEjercito2()+ "\u001B[0m");
+
+			System.out.println("\u001B[32m" + "Ejercito 1: \n " + terreno.getEjercito1().mayorVidaEjercito()+"\u001B[0m");
+
+			System.out.println("\u001B[31m" + "Ejercito 2: \n" + terreno.getEjercito2().mayorVidaEjercito()+"\u001B[0m");
+
+
+			System.out.println("\u001B[32m" + "Ranking de poder ejercito 1"+"\u001B[0m");
+			terreno.getEjercito1().ranking();
+
+			System.out.println("\u001B[31m" + "Ranking de poder ejercito 2"+"\u001B[0m");
+			terreno.getEjercito2().ranking();
+			
+			ganador(terreno.getEjercito1().promedioPuntosEjercito(), terreno.getEjercito2().promedioPuntosEjercito());
+			System.out.println("Desea salir?");
+			end = sc.next();
+		}while (!end.equals("si"));
+	}
+	public static void ganador(double vida1, double vida2){
+		System.out.println("El promedio de vida del ejercito 1 es " + vida1);
+		System.out.println("El promedio de vida del ejercito 2 es " + vida2);
+		if(vida1 > vida2){
+			System.out.println("El ejercito 1 gana el juego");
+		}else if(vida2 > vida1){
+			System.out.println("El ejercito 2 gana el juego");
+		}else{
+			System.out.println("Empate");
+		}
+	}
+}
+
+
+
