1b0183a7d759be13b64a5785e4a9b3b7b1682189
mvelasquea@unsa.edu.pe <mvelasquea@unsa.edu.pe>
2024-01-10 10:23:36 -0500

    modificando la clase que creara el ejercito

--- fp2-23b/fase03/lab20/Ejercito.java
+++ fp2-23b/fase03/lab20/Ejercito.java
@@ -1,1 +1,103 @@
-creando los archivos nsesasrios para iniciar
+package problemas_ejercicio22_diciembre;
+
+import java.util.*;
+public class Ejercito{
+	/*
+	Ejercicio lab 20 
+	>	clase Ejercito (donde se creara los ejercitos de ambos bandos tanto A como B)
+	Autor :Mikhail Gabino Velasque Arcos
+	tiempo:---
+	*/
+	private ArrayList<Soldado> misSoldados = new ArrayList<Soldado>();
+	public ArrayList<Soldado> getSoldados(){
+		return misSoldados;
+	}
+	public int getCantidadEsp(){
+		int ans = 0;
+		for(Soldado s: misSoldados){
+			if(s instanceof Espadachin){
+				ans++;
+			}
+		}
+		return ans;
+	}
+	public int getCantidadArq(){
+		int ans = 0;
+		for(Soldado s: misSoldados){
+			if(s instanceof Arquero){
+				ans++;
+			}
+		}
+		return ans;
+	}
+	public int getCantidadCab(){
+		int ans = 0;
+		for(Soldado s: misSoldados){
+			if(s instanceof Caballero){
+				ans++;
+			}
+		}
+		return ans;
+	}
+	public int getCantidadLan(){
+		int ans = 0;
+		for(Soldado s: misSoldados){
+			if(s instanceof Lancero){
+				ans++;
+			}
+		}
+		return ans;
+	}
+	public String toString(){
+		String datos = "El ejercito posee " + misSoldados.size()+"\n";
+		String datosSol= genDatosSoldado();
+		return datos + datosSol;
+	}
+	private String genDatosSoldado() {
+		String ans ="Datos de los soldados\n";
+		for(Soldado c: misSoldados){
+			ans+=c+"\n";
+		}
+		return ans;
+	}
+
+	public double promedioPuntosEjercito(){
+		double sum=0;
+		for(Soldado s: misSoldados){
+			sum+=s.getVidaActual();
+		}
+		return (sum/misSoldados.size());
+	}
+	public String mayorVidaEjercito(){
+		int max = 0;
+		int index = 0;
+		int count = 0;
+		for(Soldado s: misSoldados){
+			max = Math.max(max, s.getVidaActual());
+			if(max == s.getVidaActual()){
+				index = count;
+			}
+			count++;
+		}
+		return ("El soldado con mayor vida es " + misSoldados.get(index));
+	}
+	public void ranking(){
+		int n = misSoldados.size();
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n - 1 - i; j++) {
+				if(misSoldados.get(j).getVidaActual() < misSoldados.get(j+1).getVidaActual()){
+					Soldado temp = misSoldados.get(j);
+					misSoldados.set(j, misSoldados.get(j + 1));
+					misSoldados.set(j + 1, temp);
+
+				}
+			}
+		}
+		System.out.println("Soldados ordenados por ranking de vida");
+		for(Soldado s: misSoldados){
+			System.out.println("Nombre: "+ s.getNombre() + "\tVida: " + s.getVidaActual());
+		}
+	}
+}
+
+
